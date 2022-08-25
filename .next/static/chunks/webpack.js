ce25b4ff193ab0c42b4e0ad0bb0ad339216dab1b
Felipe <felipe.bmost@gmail.com>
2022-08-25 14:21:23 -0300

    update altura main

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d856f94de687e2c1"; }
+/******/ 		__webpack_require__.h = function() { return "96bbb7f191edfe91"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
